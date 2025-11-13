86909a5aa62c5c75ada053bbc1c52c54c7e833eb
Keerthana1125 <reddikeerthanaa@gmail.com>
2025-11-13 19:02:25 +0530

    Time: 1 ms (50.53%), Space: 17.7 MB (89.56%) - LeetHub

---
+++ 0857-positions-of-large-groups/0857-positions-of-large-groups.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largeGroupPositions(self, s: str) -> List[List[int]]:
+            result = []
+            start = 0
+            while start < len(s):
+                end = start
+                while end < len(s) and s[end] == s[start]:
+                    end += 1
+                if end - start >= 3:
+                    result.append([start, end - 1])
+                start = end
+            return result
